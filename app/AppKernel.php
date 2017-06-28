666aa5dd06c3cd4b9fa3b404a0d1a8604f843f5c
sidczak <sidczak@clickmeeting.com>
2017-06-28 16:32:09 +0200

    add BlogBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new CodeExplorerBundle\CodeExplorerBundle(),
+            new BlogBundle\BlogBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
