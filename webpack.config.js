564b80ab45233020f707f189d83de54bd59cec49
sidczak <sidczak@clickmeeting.com>
2018-12-10 14:45:38 +0100

    add home.js and home.scss

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/vue/app.js')
+    .addEntry('js/home', './assets/js/home.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
+    .addStyleEntry('css/home', './assets/css/home.scss')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
