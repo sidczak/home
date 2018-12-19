2fd8bfee3db2d4c7f8a5959a893b5ae9b7486b6c
sidczak <sidczak@clickmeeting.com>
2018-12-19 23:59:22 +0100

    add example3 - method

--- src/VueBundle/Controller/VueBasicController.php
+++ src/VueBundle/Controller/VueBasicController.php
@@ -21,7 +21,14 @@ class VueBasicController extends Controller
     public function example2Action()
     {
         return $this->render('VueBundle:VueBasic:example2.html.twig',[
-			'title' => 'Welcome Vue Basic - example2'
+			'title' => 'Welcome Vue Basic - example2 - Hello World z Vue'
+        ]);
+    }
+
+    public function example3Action()
+    {
+        return $this->render('VueBundle:VueBasic:example3.html.twig',[
+			'title' => 'Welcome Vue Basic - example3 - metody'
         ]);
     }
 }
