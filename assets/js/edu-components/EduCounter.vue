911899fbae6f6d0295367636f0930babf31262ae
sidczak <sidczak@clickmeeting.com>
2019-01-23 16:44:37 +0100

    update EduCounter

--- assets/js/edu-components/EduCounter.vue
+++ assets/js/edu-components/EduCounter.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <form action="#">
+<!--         <form action="#">
             <div class="form-group row">
                 <div class="col-2">
                     <label class="form-label">Wybierz wartość startową:</label>
@@ -11,7 +11,7 @@
                     </select>
                 </div>
             </div>
-        </form>
+        </form> -->
         <div class="row justify-content-center">
             <div class="col-8 text-center">
                 <div class="alert alert-primary" v-show="!inProgress">Naciśnij <strong>Start</strong> aby rozpocząć odliczanie.</div>
@@ -27,14 +27,9 @@
         props: ["initValue"],
         data: function() {
             return {
-                counter: this.initValue ? this.initValue : 10,
+                counter: this.initValue,
                 inProgress: false
             };
-        },
-        watch: {
-            initValue: function(newValue) {
-                this.counter = newValue;
-            }
         },
         methods: {
             start: function() {
@@ -42,7 +37,9 @@
                 this.countdown();
             },
             countdown: function() {
+
                 this.counter--;
+
                 if(this.counter > 0) {
                     setTimeout(this.countdown, 1000);
                 } else {
@@ -51,9 +48,9 @@
                         this.inProgress = false;
                     }, 1000);
                 }
+
             }
         }
-        
     };
 </script>
 <style lang="scss" scoped>
